15a9d84faedec540ee6cde49c8bd160b93efe70a
Pen <vusiphu20042000@gmail.com>
2023-03-21 02:17:30 +0700

    commit2

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,7 +1,20 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export default function NewsList() {
+    const [openDialog, setOpenDialog] = React.useState(false);
+    const [deleteId, setDeleteId] = React.useState(0);
+
+
+    const handleClickOpenDialog = (id) => {
+        setOpenDialog(true);
+        setDeleteId(id);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
     const [news, setNews] = useState([]);
     useEffect(() => {
         fetch(`https://6418728e75be53f451dfc104.mockapi.io/news`)
@@ -11,12 +24,12 @@ export default function NewsList() {
             })
             .catch((error) => console.log(error));
     }, []);
-    const handleDelete = (id) => {
-        fetch(`https://6418728e75be53f451dfc104.mockapi.io/news/${id}`, {
+    const handleDelete = () => {
+        fetch(`https://6418728e75be53f451dfc104.mockapi.io/news/${deleteId}`, {
             method: 'DELETE',
         }).then((response) => response.json())
             .then((data) => {
-                alert('Delete Successfully')
+                setOpenDialog(false);
                 fetch(`https://6418728e75be53f451dfc104.mockapi.io/news`)
                     .then((response) => response.json())
                     .then((data) => {
@@ -24,6 +37,7 @@ export default function NewsList() {
                     })
                     .catch((error) => console.log(error));
                 // return ((window.location.href = `/dashboard`))
+                alert('Delete successfully!')
             })
             .catch((error) => console.log(error));
     };
@@ -44,11 +58,32 @@ export default function NewsList() {
                             <td>{item.description}</td>
                             <td>
                                 <Link to={`/news/${item.id}`}>View</Link>
-                                <Link to={`/news/${item.id}/edit`}>Edit</Link><button onClick={() => handleDelete(item.id)}>Delete</button>
+                                <Link to={`/news/${item.id}/edit`}>Edit</Link>
+                                <Button onClick={() => handleClickOpenDialog(item.id)}>Delete</Button>
                             </td>
                         </tr>
                     ))}
                 </tbody>
-            </table></div>
+            </table>
+            {<Dialog
+                open={openDialog}
+                onClose={handleCloseDialog}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Are you sure to Delete this new ?"}
+                </DialogTitle>
+                {/* <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+
+                    </DialogContentText>
+                </DialogContent> */}
+                <DialogActions>
+                    <Button onClick={handleCloseDialog}>Disagree</Button>
+                    <Button onClick={handleDelete}>Agree</Button>
+                </DialogActions>
+            </Dialog>}
+        </div>
     )
 }
